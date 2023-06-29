522b670eb5b061d6a8c901c5d48e2dffa1224778
YashPurohit047 <f20200946@pilani.bits-pilani.ac.in>
2023-06-29 14:28:10 +0530

    Initial commit

---
+++ src/edufair_registration_window/class_xii_ip_project/EDUFAIR_REGISTRATION_WINDOWClass_XII_IP_Project.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edufair_registration_window.class_xii_ip_project;
+
+/**
+ *
+ * @author YASH
+ */
+public class EDUFAIR_REGISTRATION_WINDOWClass_XII_IP_Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
